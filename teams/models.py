5ce844705637a47ed7445fe8fa34f5db371ac7b7
Robert <robikoser@gmail.com>
2020-09-24 18:55:50 +0200

    Add pre-commit

--- teams/models.py
+++ teams/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
+
 class Team(models.Model):
     name = models.CharField('Name', max_length=128)
-
-class Test:
-    pass
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import url
-from django.urls import include, path
+from django.urls import path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from users import views
 
