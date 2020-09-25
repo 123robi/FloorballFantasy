f892f054b1416738eb7d28950d9c1c2f42b4ed05
Robert <robikoser@gmail.com>
2020-09-25 13:29:31 +0200

    Add floorball team model

---
+++ floorball_teams/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FloorballTeamsConfig(AppConfig):
+    name = 'floorball_teams'
---
+++ floorball_teams/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class FloorballTeam(models.Model):
+    name = models.CharField('Name', max_length=128)
+    wins = models.PositiveIntegerField()
+    loses = models.PositiveIntegerField()
+    wins_after_over_time = models.PositiveIntegerField()
+    loses_after_over_time = models.PositiveIntegerField()
+    points = models.PositiveIntegerField()
